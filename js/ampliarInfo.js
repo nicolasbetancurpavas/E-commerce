8c2f2739f76ad22b901903cfe0f37651ad6aa8e3
Your Name <betancur10nicolas@gmail.com>
2022-09-01 17:23:46 -0500

    EV17 agregando mejora scroll y modal informacion

---
+++ js/ampliarInfo.js
@@ -0,0 +1,44 @@
+export function ampliarInformacionProducto(e) {
+
+    let productos = {}
+
+ 
+
+    //llenar informacion del producto
+    productos.foto=e.target.parentElement.querySelector(".card-img").src
+    productos.nombre=e.target.parentElement.querySelector(".card-nombre").textContent
+    productos.precioAntes=e.target.parentElement.querySelector(".precio-anterior").textContent
+    productos.precioAhora = e.target.parentElement.querySelector(".precio-nuevo").textContent
+
+    // relleno modal
+    const imagen = document.querySelector(".img-informacion-producto")
+    imagen.src = e.target.parentElement.querySelector(".card-img").src
+
+    const nombre = document.querySelector(".nombre-modal")
+    nombre.textContent = e.target.parentElement.querySelector(".card-nombre").textContent
+
+    const preciAnterior = document.querySelector(".precio-antes-modal")
+    preciAnterior.textContent = e.target.parentElement.querySelector(".precio-anterior").textContent
+
+    const precioNuevo = document.querySelector(".precio-nuevo-modal")
+    precioNuevo.textContent = `$ ${e.target.parentElement.querySelector(".precio-nuevo").textContent}`
+
+    const mostrarprecio = document.querySelector(".precio-producto-modal")
+    mostrarprecio.textContent = e.target.parentElement.querySelector(".precio-nuevo").textContent
+
+
+
+    // eliminar vista de los elementos sin descuento
+    if(preciAnterior.textContent == 0){
+        preciAnterior.style.display = "none"
+    }
+    if(preciAnterior.textContent > 0){
+        preciAnterior.style.display = "block"
+    }
+
+  
+
+    return productos
+
+
+}
--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,25 @@
-const productos = document.querySelector(".main-productos")
+import llenarTienda from "./traversing.js"
+import { ampliarInformacionProducto } from "./ampliarInfo.js"
 
-productos.addEventListener("click",(e)=>{
-    if(e.target.classList.contains("card-btn")){
-        console.log("oprimiendo boton")
+llenarTienda()
+
+let productos = {}
+
+// llamo modal
+
+const modalInformacion = new bootstrap.Modal(document.getElementById("exampleModal"))
+
+const contenedorProductos = document.querySelector(".main-productos")
+
+// dectetamos contenedor de todos los productos
+// sacamos informacion del contendor en especifico 
+
+contenedorProductos.addEventListener("click", (e) => {
+    if (e.target.classList.contains("card-btn")) {
+        productos = ampliarInformacionProducto(e)
+        modalInformacion.show()
     }
 })
+
+
+
