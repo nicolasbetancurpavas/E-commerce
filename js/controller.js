d60e07b41c6541414677cd8733e152340b2fd3a6
Your Name <betancur10nicolas@gmail.com>
2022-09-05 19:04:49 -0500

    EV20 agregando modal resumen de compras y mejorando diseno

--- js/controller.js
+++ js/controller.js
@@ -15,8 +15,6 @@ const contenedorProductos = document.querySelector(".main-productos")
 // dectetamos contenedor de todos los productos
 // sacamos informacion del contendor en especifico 
 
-let URLactual = window.location
-
 contenedorProductos.addEventListener("click", (e) => {
     if (e.target.classList.contains("card-btn") || e.target.classList.contains("card-img")) {
         // valor cantidad vuelve a 1 
@@ -27,7 +25,6 @@ contenedorProductos.addEventListener("click", (e) => {
         modalInformacion.show()
     }
 })
-
 
 // rutina para agregar al carrito
 
@@ -41,23 +38,123 @@ btnAdd.addEventListener("click", () => {
     carrito.push(productos)
 
     // saco cantidades de cada producto y las sumo 
-    let cantidades = carrito.map((e)=>e.cantidad)
-    let suma = cantidades.reduce((indiceActual,indiceSiguiente)=>indiceActual + indiceSiguiente)
-    
+    let cantidades = carrito.map((e) => e.cantidad)
+    let suma = cantidades.reduce((indiceActual, indiceSiguiente) => indiceActual + indiceSiguiente)
+
     capsula.textContent = suma
-    capsula.classList.remove("invisible") 
-    
-    modalInformacion.hide() 
+    capsula.classList.remove("invisible")
+
+    modalInformacion.hide()
+})
+
+
+
+// limpiar carrito 
+
+let limpiar = document.querySelector(".limpiar-modal")
+console.log(limpiar)
+limpiar.addEventListener("click", () => {
+    carrito = []
+    capsula.classList.add("invisible")
+    modalResumen.hide()
 })
 
 // modal compras agregadas 
-let iconShop  = document.querySelector(".shop")
+let iconShop = document.querySelector(".shop")
 
-iconShop.addEventListener("click",()=>{
+iconShop.addEventListener("click", () => {
+
+    // contenedor base para pintar resumen compra
+    let baseResumenCompra = document.querySelector(".container-resumen-compra")
+    console.log(carrito)
+
+    baseResumenCompra.innerHTML = ""
+
+    // verificamos que haya productos agregados
+    let nullProductos = document.querySelector(".NA")
+    if (carrito.length === 0) {
+        let totalneto = document.querySelector(".productnull")
+        nullProductos.classList.remove("desactivar")
+        totalneto.textContent = 0
+
+    }
+    if (carrito.length > 0) {
+        nullProductos.classList.add("desactivar")
+    }
+
+
+
+    // pintamos los productos 
+    carrito.forEach((producto) => {
+
+        let contenedor = document.createElement("div")
+        contenedor.classList.add("item-producto-resumen")
+
+        let figure = document.createElement("figure")
+        figure.classList.add("figure")
+
+        let img = document.createElement("img")
+        img.classList.add("img-resumen-modal")
+        img.src = producto.foto
+
+        let divInformacion = document.createElement("div")
+        divInformacion.classList.add("container-info-product")
+
+        let nombre = document.createElement("h2")
+        nombre.classList.add("nombre-resumen-modal")
+        nombre.textContent = producto.nombre
+
+        let precioAntes = document.createElement("h5")
+        precioAntes.classList.add("precio-antes-resumen-modal")
+        precioAntes.textContent = producto.precioAntes
+
+        let precioAhora = document.createElement("h3")
+        precioAhora.classList.add("precio-actual-resumen-modal")
+        precioAhora.textContent = producto.precioAhora
+
+        let cantidad = document.createElement("h5")
+        cantidad.classList.add("cantidad-resumen-modal")
+        cantidad.textContent = `Cantidad: ${producto.cantidad}`
+
+        let divEnviogratis = document.createElement("div")
+        divEnviogratis.classList.add("container-envio-gratis")
+
+        let IconoGratis = document.createElement("i")
+        IconoGratis.classList.add("fa-solid")
+        IconoGratis.classList.add("fa-truck")
+
+        let envioGratis = document.createElement("p")
+        envioGratis.classList.add("envio-gratiss")
+        envioGratis.textContent = "envio gratis"
+
+        baseResumenCompra.appendChild(contenedor)
+        contenedor.appendChild(figure)
+        figure.appendChild(img)
+        contenedor.appendChild(divInformacion)
+        divInformacion.appendChild(nombre)
+        divInformacion.appendChild(precioAntes)
+        divInformacion.appendChild(precioAhora)
+        divInformacion.appendChild(cantidad)
+        divInformacion.appendChild(divEnviogratis)
+        divEnviogratis.appendChild(IconoGratis)
+        divEnviogratis.appendChild(envioGratis)
+
+        // agrego atributo el subtotal
+        producto.subtotal = Number(producto.precioAhora) * Number(producto.cantidad)
+
+        // btn calcular compra 
+        let calcular = document.querySelector(".buy")
+        let totalneto = document.querySelector(".productnull")
+
+        calcular.addEventListener("click", () => {
+            let sub = carrito.map((elemeto) => elemeto.subtotal)
+            let TOTAL = sub.reduce((actual, siguiente) => actual + siguiente)
+            console.log(sub)
+            console.log(TOTAL)
+            totalneto.textContent = `Total compra: ${TOTAL}`
+        })
+
+    })
+
     modalResumen.show()
 })
-
-
-
-
-
